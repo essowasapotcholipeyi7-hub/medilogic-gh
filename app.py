6d7f6e58fae86093ac3848ca4197f23dc809b566
essowasainfo60-beep <essowasainfo60@gmail.com>
2026-07-10 23:34:54 +0000

    ajout de from flask mail

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask_mail import Mail, Message
 from config import Config
 from sheets_helper import sheets_helper
 import hashlib
